dd95c8a22f8f6a057a706d59f1f4ba9e019df2d2
Gerbut Evgeny <gekas@i.ua>
2017-04-16 10:33:37 +0300

    222

--- ChislennieMethody_Lab1/Program.cs
+++ ChislennieMethody_Lab1/Program.cs
@@ -11,9 +11,9 @@ namespace ChislennieMethody_Lab1
     {
         static void Main(string[] args)
         {
-            //HalfDeviation method = new HalfDeviation(2, 3, (x) => { return x*x*x - 3*x*x + 4*x - 5; });
-            //SimpleIteration method1 = new SimpleIteration(2, 3, 0.00001, (x) => { return x * x * x - 3 * x * x + 4 * x - 5; }, (x) => { return 3 * x * x - 6 * x + 4; });
-            //Chords method2 = new Chords(2, 3, 0.00001, (x) => { return x * x * x - 3 * x * x + 4 * x - 5; }, (x) => { return 3 * x * x - 6 * x + 4; }, (x) => { return 6 * x - 6; });
+            HalfDeviation method = new HalfDeviation(2, 3, (x) => { return x * x * x - 3 * x * x + 4 * x - 5; });
+            SimpleIteration method1 = new SimpleIteration(2, 3, 0.00001, (x) => { return x * x * x - 3 * x * x + 4 * x - 5; }, (x) => { return 3 * x * x - 6 * x + 4; });
+            Chords method2 = new Chords(2, 3, 0.00001, (x) => { return x * x * x - 3 * x * x + 4 * x - 5; }, (x) => { return 3 * x * x - 6 * x + 4; }, (x) => { return 6 * x - 6; });
             Newton method3 = new Newton(2, 3, 0.00001, (x) => { return x * x * x - 3 * x * x + 4 * x - 5; }, (x) => { return 3 * x * x - 6 * x + 4; }, (x) => { return 6 * x - 6; });
 
             Console.ReadLine();
--- ChislennieMethody_Lab2/Program.cs
+++ ChislennieMethody_Lab2/Program.cs
@@ -10,17 +10,19 @@ namespace ChislennieMethody_Lab2
     {
         static void Main(string[] args)
         {
-            //var xx = Gaus.Calculate(matrix);
-
-            //for(int i =0; i < xx.Length;i++)
-            //    Console.WriteLine(xx[i]);
-
             double[,] array = { { 2,-1,-1},
                                  { 3,4,-2 },
                                  { 3,-2,4}
                                };
 
-            Holetski h = new Holetski(array);
+            var xx = Gaus.Calculate(array);
+
+            for (int i = 0; i < xx.Length; i++)
+                Console.WriteLine(xx[i]);
+
+
+
+            //Holetski h = new Holetski(array);
             Console.ReadLine();
 
         }
